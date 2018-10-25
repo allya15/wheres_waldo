cc52c2ec7a61632136f7ec1dffdb63a75bd921fd
allya15 <allison.avis@gmail.com>
2018-10-25 18:38:17 +0000

    completed array foreach exercise

---
+++ actionWhenFound.js
@@ -0,0 +1,22 @@
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);
+//     }
+//   }
+// }
+
+
+function findWaldo(arr, found) {
+  arr.forEach(function(found, index) {
+    if (arr[index] === "Waldo") {
+      actionWhenFound(index);
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index", index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
